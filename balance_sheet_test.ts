f07ccfad36849ad3a3eb80ba2018e2eb3344b529
James Oguntebi <jaoguntebi@gmail.com>
2020-07-16 15:18:42 -0400

    Improve BalanceSheet test. Add a todo.

--- balance_sheet_test.ts
+++ balance_sheet_test.ts
@@ -1,4 +1,4 @@
-import Config from "./config";
+import Config, { ConfigParams } from "./config";
 import { Test } from "./testing/testrunner";
 import { Tester } from "./testing/tester";
 import BalanceSheet, { BalanceRow } from "./balance_sheet";
@@ -12,27 +12,47 @@ export default class BalanceSheetTest implements Test {
         t.spyOn(BalanceSheet, 'insertRow');
       });
 
-      t.describe('for rent config', () => {
-        t.beforeEach(() => {
-          t.setConfig(Config.getRentConfigForTest({
+      const configTypes = {
+        'rent config': {
+          config: Config.getRentConfigForTest({
             rentConfig: {
               dueDayOfMonth: new Date().getDate(),
               monthlyAmount: 873,
             },
-          }));
-        });
+          }),
+          expectedDecription: 'Rent due',
+          expectedTransaction: -873,
+        },
+        'loan config': {
+          config: Config.getLoanConfigForTest({
+            loanConfig: {
+              interestDayOfMonth: new Date().getDate(),
+              interestRate: 0.06,
+            },
+          }),
+          expectedDecription: 'Monthly interest',
+          expectedTransaction: 'interest',
+        }
+      };
 
-        t.it('inserts a row on due day', () => {
-          BalanceSheet.maybeAddRentOrInterestTransaction();
-          t.expect(BalanceSheet.insertRow).toHaveBeenCalledLike(t.matcher(
-              (args: unknown[]) => {
-                const rowArgs = args[0] as BalanceRow;
-                t.expect(rowArgs.description).toEqual('Rent due');
-                t.expect(rowArgs.transaction).toEqual(-873);
-                return true;
-              }));
+      for (const configType in configTypes) {
+        t.describe(`for ${configType}`, () => {
+          const {config, expectedDecription, expectedTransaction} =
+              configTypes[configType];
+          t.beforeEach(() => t.setConfig(config));
+  
+          t.it('inserts a row on due day', () => {
+            BalanceSheet.maybeAddRentOrInterestTransaction();
+            t.expect(BalanceSheet.insertRow).toHaveBeenCalledLike(t.matcher(
+                (args: unknown[]) => {
+                  const rowArgs = args[0] as BalanceRow;
+                  t.expect(rowArgs.description).toEqual(expectedDecription);
+                  t.expect(rowArgs.transaction).toEqual(expectedTransaction);
+                  return true;
+                }));
+          });
         });
-      });
+      }
     });
   }
 }
