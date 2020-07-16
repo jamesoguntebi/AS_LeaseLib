e305604faf60351dd18e920da3986ce9a620f4a2
James Oguntebi <jaoguntebi@gmail.com>
2020-07-16 18:38:45 -0400

    Add a test to BalanceSheetTest. Clean up EmailSenderTest.

--- balance_sheet_test.ts
+++ balance_sheet_test.ts
@@ -6,50 +6,118 @@ import BalanceSheet, { BalanceRow } from "./balance_sheet";
 export default class BalanceSheetTest implements Test {
   readonly name = 'BalanceSheetTest';
 
-  run(t: Tester) {
-    t.describe('maybeAddRentOrInterestTransaction', () => {
-      t.beforeEach(() => {
-        t.spyOn(BalanceSheet, 'insertRow');
-      });
+  private expectInsertRowToHaveBeenCalledLike(
+      t: Tester, matcher: (params: BalanceRow) => boolean) {
+    t.expect(BalanceSheet.insertRow).toHaveBeenCalledLike(
+      t.matcher((args: unknown[]) => {
+        return matcher((args as Parameters<typeof BalanceSheet.insertRow>)[0]);
+      }));
+  }
 
-      const configTypes = {
-        'rent config': {
-          config: Config.getRentConfigForTest({
-            rentConfig: {
-              dueDayOfMonth: new Date().getDate(),
-              monthlyAmount: 873,
-            },
-          }),
+  run(t: Tester) {
+    const baseConfigSpecs = [
+      {
+        configType: 'rent config',
+        config: Config.getRentConfigForTest({
+          rentConfig: {
+            dueDayOfMonth: new Date().getDate(),
+            monthlyAmount: 873,
+          },
+        }),
+      },
+      {
+        configType: 'loan config',
+        config: Config.getLoanConfigForTest({
+          loanConfig: {
+            interestDayOfMonth: new Date().getDate(),
+            interestRate: 0.06,
+          },
+        }),
+      }
+    ];
+
+    t.describe('maybeAddRentOrInterestTransaction', () => {
+      t.beforeEach(() => t.spyOn(BalanceSheet, 'insertRow'));
+
+      const configSpecs = [
+        {
+          ...baseConfigSpecs[0],
           expectedDecription: 'Rent due',
           expectedTransaction: -873,
         },
-        'loan config': {
-          config: Config.getLoanConfigForTest({
-            loanConfig: {
-              interestDayOfMonth: new Date().getDate(),
-              interestRate: 0.06,
-            },
-          }),
+        {
+          ...baseConfigSpecs[1],
           expectedDecription: 'Monthly interest',
-          expectedTransaction: 'interest',
+          expectedTransaction: 'interest' as const,
         }
-      };
+      ];
 
-      for (const configType in configTypes) {
+      for (const typeObj of configSpecs) {
+        for (const replaceDate of [true, false]) {
+          const dateString =
+              replaceDate ? `not on transaction day` : `on transaction day`;
+          const {configType, config, expectedDecription, expectedTransaction} =
+              typeObj;
+
+          t.describe(`for ${configType} ${dateString}`, () => {
+            t.beforeEach(() => {
+              t.setConfig(config);
+              if (replaceDate) {
+                // Replace the day of month with some other day.
+                const fakeMonthDay = (new Date().getDate() + 1) % 28 + 1;
+                t.spyOn(Date.prototype, 'getDate').and.returnValue(
+                    fakeMonthDay);
+              }
+            });
+    
+            const testString =
+                `${replaceDate ? 'does not insert' : 'inserts'} a row on ` +
+                `due day`;
+            t.it(testString, () => {
+              BalanceSheet.maybeAddRentOrInterestTransaction();
+              if (replaceDate) {
+                t.expect(BalanceSheet.insertRow).toNotHaveBeenCalled();
+              } else {
+                this.expectInsertRowToHaveBeenCalledLike(
+                  t, (row: BalanceRow) => {
+                    t.expect(row.description).toEqual(expectedDecription);
+                    t.expect(row.transaction).toEqual(expectedTransaction);
+                    return true;
+                  });
+              }
+            });
+          });
+        }
+      }
+    });
+
+    t.describe('addPayment', () => {
+      t.beforeEach(() => t.spyOn(BalanceSheet, 'insertRow'));
+
+      const configSpecs = [
+        {
+          ...baseConfigSpecs[0],
+          expectedDecription: 'Rent payment',
+        },
+        {
+          ...baseConfigSpecs[1],
+          expectedDecription: 'Loan payment',
+        }
+      ];
+
+      for (const {configType, config, expectedDecription} of configSpecs) {
         t.describe(`for ${configType}`, () => {
-          const {config, expectedDecription, expectedTransaction} =
-              configTypes[configType];
           t.beforeEach(() => t.setConfig(config));
   
-          t.it('inserts a row on due day', () => {
-            BalanceSheet.maybeAddRentOrInterestTransaction();
-            t.expect(BalanceSheet.insertRow).toHaveBeenCalledLike(t.matcher(
-                (args: unknown[]) => {
-                  const rowArgs = args[0] as BalanceRow;
-                  t.expect(rowArgs.description).toEqual(expectedDecription);
-                  t.expect(rowArgs.transaction).toEqual(expectedTransaction);
-                  return true;
-                }));
+          t.it('inserts a row', () => {
+            BalanceSheet.addPayment(159, new Date());
+
+            this.expectInsertRowToHaveBeenCalledLike(
+              t, (row: BalanceRow) => {
+                t.expect(row.description).toEqual(expectedDecription);
+                t.expect(row.transaction).toEqual(159);
+                return true;
+              });
           });
         });
       }
--- email_sender_test.ts
+++ email_sender_test.ts
@@ -9,7 +9,7 @@ type SendEmailParameters = Parameters<typeof GmailApp.sendEmail>;
 export default class EmailSenderTest implements Test{
   readonly name: string = 'EmailSenderTest';
 
-  private assertWithSendEmailParams(
+  private expectSendMailToHaveBeenCalledLike(
       t: Tester, matcher: (params: SendEmailParameters) => boolean) {
     t.expect(GmailApp.sendEmail).toHaveBeenCalledLike(
       t.matcher((args: unknown[]) => matcher(args as SendEmailParameters)));
@@ -29,86 +29,54 @@ export default class EmailSenderTest implements Test{
       t.it('formats large numbers with comma', () => {
         EmailSender.sendPaymentThanks(1500);
 
-        this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-          t.expect(params[2]).toContain('1,500');
-          t.expect(params[3].htmlBody).toContain('1,500');
-          return true;
-        });
+        this.expectSendMailToHaveBeenCalledLike(
+            t, (params: SendEmailParameters) => {
+              t.expect(params[2]).toContain('1,500');
+              t.expect(params[3].htmlBody).toContain('1,500');
+              return true;
+            });
       });
 
       const RED_BALANCE_STRING = 'style="color: #b34;"';
 
-      t.describe('with positive balance', () => {
-        t.beforeEach(() => {
-          t.spyOn(BalanceSheet, 'getBalance').and.returnValue(100);
-        });
+      const balanceSpecs = [
+        {balance: 100, type: 'positive'},
+        {balance: -100, type: 'negative'},
+      ];
+      const configSepcs = [
+        {type: 'rent', config: Config.getRentConfigForTest()},
+        {type: 'loan', config: Config.getLoanConfigForTest()},
+      ];
 
-        t.describe('with loan config', () => {
-          t.beforeEach(() => {
-            t.setConfig(Config.getLoanConfigForTest());
-          });
-
-          t.it('does not show balance in red', () => {
-            EmailSender.sendPaymentThanks(1);
-    
-            this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-              t.expect(params[3].htmlBody).toNotContain(RED_BALANCE_STRING);
-              return true;
-            });
-          });
-        });
-
-        t.describe('with rent config', () => {
-          t.beforeEach(() => {
-            t.setConfig(Config.getRentConfigForTest());
-          });
-
-          t.it('shows balance in red', () => {
-            EmailSender.sendPaymentThanks(1);
-    
-            this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-              t.expect(params[3].htmlBody).toContain(RED_BALANCE_STRING);
-              return true;
-            });
-          });
-        });
-      });
-
-      t.describe('negative balance', () => {
-        t.beforeEach(() => {
-          t.spyOn(BalanceSheet, 'getBalance').and.returnValue(-100);
-        });
-
-        t.describe('with loan config', () => {
-          t.beforeEach(() => {
-            t.setConfig(Config.getLoanConfigForTest());
-          });
-
-          t.it('does not show balance in red', () => {
-            EmailSender.sendPaymentThanks(1);
-    
-            this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-              t.expect(params[3].htmlBody).toNotContain(RED_BALANCE_STRING);
-              return true;
-            });
-          });
-        });
-
-        t.describe('with rent config', () => {
-          t.beforeEach(() => {
-            t.setConfig(Config.getRentConfigForTest());
-          });
-
-          t.it('does not show balance in red', () => {
-            EmailSender.sendPaymentThanks(1);
-    
-            this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-              t.expect(params[3].htmlBody).toNotContain(RED_BALANCE_STRING);
-              return true;
-            });
-          });
-        });
-      });
+      for (const {type: balanceType, balance} of balanceSpecs) {
+        for (const {type: configType, config} of configSepcs) {
+          t.describe(`when showing ${balanceType} balance for ${configType}`,
+              () => {
+                t.beforeEach(() => {
+                  t.setConfig(config);
+                  t.spyOn(BalanceSheet, 'getBalance').and.returnValue(balance);
+                });
+                
+                const expectRed = configType === 'rent' && balance > 0;
+                const testName = expectRed ?
+                    `shows balance in red` : `does not show balance in red`;
+                t.it(testName, () => {
+                  EmailSender.sendPaymentThanks(1);
+          
+                  this.expectSendMailToHaveBeenCalledLike(
+                      t, (params: SendEmailParameters) => {
+                        const expectation = t.expect(params[3].htmlBody);
+                        if (expectRed) {
+                          expectation.toContain(RED_BALANCE_STRING);
+                        } else {
+                          expectation.toNotContain(RED_BALANCE_STRING);
+                        }
+                        return true;
+                      });
+                });
+              });
+        }
+      }
 
       t.describe('for config fields', () => {
         t.beforeEach(() => {
@@ -118,16 +86,18 @@ export default class EmailSenderTest implements Test{
         t.it('uses them', () => {
           EmailSender.sendPaymentThanks(1);
           
-          this.assertWithSendEmailParams(t, (params: SendEmailParameters) => {
-            t.expect(params[0]).toEqual(
-                Config.DEFAULT.customerEmails.join(', '));
-            t.expect(params[3].cc).toEqual(
-                Config.DEFAULT.emailCCs.join(', '));
-            t.expect(params[3].bcc).toEqual(
-                Config.DEFAULT.emailBCCs.join(', '));
-            t.expect(params[3].name).toEqual(Config.DEFAULT.emailDisplayName);
-            return true;
-          });
+          this.expectSendMailToHaveBeenCalledLike(
+              t, (params: SendEmailParameters) => {
+                t.expect(params[0]).toEqual(
+                    Config.DEFAULT.customerEmails.join(', '));
+                t.expect(params[3].cc).toEqual(
+                    Config.DEFAULT.emailCCs.join(', '));
+                t.expect(params[3].bcc).toEqual(
+                    Config.DEFAULT.emailBCCs.join(', '));
+                t.expect(params[3].name).toEqual(
+                    Config.DEFAULT.emailDisplayName);
+                return true;
+              });
         });
       });
     });
